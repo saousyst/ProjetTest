9ca783aaceae3be8de7a4f2865653ad598be4538
saousyst <saousyst@gmail.com>
2013-05-17 18:13:48 +0100

    Ajout de la classe Main

---
+++ src/saou/syst/dev/Prince.java
@@ -0,0 +1,20 @@
+/**
+ * Classe Main de demarrage 
+ */
+package saou.syst.dev;
+
+/**
+ * @author Rachid SAOU
+ *
+ */
+public class Prince {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello in ProjetTest!");
+	}
+
+}
